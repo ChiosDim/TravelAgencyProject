f3dab234fb9e8557a7ac24e6232195c3b357e624
LAPTOP-JKLD61AE\simos <hfgvxchtf@gmail.com>
2021-04-26 13:20:44 +0300

    carprice

--- src/EconomyPackage.java
+++ src/EconomyPackage.java
@@ -5,7 +5,8 @@ public class EconomyPackage {
 	private int price;
 	private int hotelstars;
 	private boolean car;
-	private int priceperday;// for car//
+	private int priceperday;
+	 private int pricecar= 20;// for car//
 	
 	
 	public  EconomyPackage(int price,int hotelstars,boolean car , int priceperday) 
@@ -14,6 +15,7 @@ public class EconomyPackage {
 		this.hotelstars=hotelstars;
 		this.car=car;
 		this.priceperday=priceperday;
+		
 	}
 	 public int  Getprice()
 	   {
@@ -23,7 +25,10 @@ public class EconomyPackage {
 	public void CalculatePrice(Travelinformation t)
 	{
 		int totalprice;
-		totalprice=price *(t.getNumber())*(t.getDays());
+		if(car==true) {
+		totalprice = price *(t.getNumber())*(t.getDays())+(pricecar*t.getDays());}
+		else {totalprice =Getprice()*(t.getNumber())*(t.getDays());
+				}
 	}
 		
 	
