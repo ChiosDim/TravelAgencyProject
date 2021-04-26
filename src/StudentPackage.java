7f74529010f74f709bbab4c2da515698a72f3898
LAPTOP-JKLD61AE\simos <hfgvxchtf@gmail.com>
2021-04-26 12:00:43 +0300

    Studentpackage

--- src/StudentPackage.java
+++ src/StudentPackage.java
@@ -1,4 +1,20 @@
 
-public class StudentPackage {
+public class StudentPackage extends EconomyPackage {
+	private int discount;
+	
+	public  StudentPackage(int price,int hotelstars,boolean car , int priceperday,int discount) 
+	{
+		super(price,hotelstars,car,priceperday);
+		this.discount=discount;
+	}
+	
+	public void CalculatePrice(Travelinformation t)
+	{
+		int totalprice;
+		int totalpricewd;
+		totalpricewd =Getprice()*(t.getNumber())*(t.getDays());
+		totalprice=totalpricewd-totalpricewd*(discount/100);
+		
+	}
 
 }
